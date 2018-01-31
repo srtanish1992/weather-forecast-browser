18ef05d9c4acb740e3d90e62e92e62cba4d5fbca
Anish Surti <srtanish1992@gmail.com>
2018-01-31 02:59:56 -0600

    added middleware redux-promise, charts and google map

---
+++ ReduxSimpleStarter/src/reducers/reducer_weather.js
@@ -0,0 +1,13 @@
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state = [], action) {
+  // console.log('Action received', action);
+
+  switch(action.type) {
+    case FETCH_WEATHER :
+      return [action.payload.data, ...state]; // ES6 syntax
+      // return state.concat([action.payload.data]);
+  }
+
+  return state;
+}
